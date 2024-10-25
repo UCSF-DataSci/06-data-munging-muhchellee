0ff1132846f38564f6535dff42df4277f5508497
Michelle <181282244+muhchellee@users.noreply.github.com>
2024-10-25 14:10:59 -0700

    Still working

--- clean_data.py
+++ clean_data.py
@@ -8,6 +8,7 @@ import numpy as np
 from numpy.random import default_rng
 import argparse
 from tqdm import tqdm
+import warnings
 
 # default input and output filenames
 DEFAULT_INPUT_FILE = 'messy_population_data.csv'
@@ -49,3 +50,36 @@ def log_transform_outliers(df, column_name):
         print(f"Column '{column_name}' does not exist in the DataFrame.")
     
     return df
+
+def drop_duplicate_rows(df):
+    """drops duplicate rows if they exist"""
+    init_row_count = df.shape[0]
+    dropped_duplicates = df.drop_duplicates()
+    final_row_count = dropped_duplicates.shape[0]
+
+    num_duplicates = init_row_count - final_row_count
+    if num_duplicates > 0:
+        print(f"{num_duplicates} duplicate rows removed.")
+    else:
+        print("No duplicate rows found.")
+
+    return dropped_duplicates
+
+def convert_to_categorical(df, columns):
+    """converts specified columns in a df to categorical type"""
+    for col in columns:
+        try:
+            if col in df.columns:
+                # check if the column is already a category
+                if isinstance(df[col].dtype, pd.CategoricalDtype):
+                    warnings.warn(f"Column '{col}' is already of type 'category'.")
+                else:
+                    # convert to categorical
+                    df[col] = df[col].astype('category')
+            else:
+                raise ValueError(f"Column '{col}' not found in DataFrame.")
+        except Exception as e:
+            print(f"Error processing column '{col}': {e}")
+    
+    return df
+
